792f6b219248b17176754ae9c1b6db7a00321574
Edward Loveall <edward@edwardloveall.com>
2014-09-23 22:05:01 -0400

    Fix regression in round simulator factory
    It was still passing a seed instead of a level

--- spec/support/round_simulator_factory.rb
+++ spec/support/round_simulator_factory.rb
@@ -1,13 +1,7 @@
 def basic_round
-  seed = SMALL_LEVEL_SEED
-  @sim = Simulator.new(seed: seed)
+  level = small_level
+  @sim = Simulator.new(level: level)
   @solution = @sim.solutions.first
   level = @sim.level
   RoundSimulator.new(solution: @solution, level: level)
-end
-
-def round_with_level(level)
-  round = RoundSimulator.new()
-  round.level = level
-  round
 end
